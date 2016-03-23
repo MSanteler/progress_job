d6d82e05d06900411b3bc3e6d6e98baf652a7457
Matthew Santeler <msanteler@gmail.com>
2016-03-23 00:04:24 -0400

    Relying on server error is messy. Explicitly return not found

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -2,10 +2,14 @@ module ProgressJob
   class ProgressController < ActionController::Base
 
     def show
-      @delayed_job = Delayed::Job.find(params[:job_id])
-      percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
-      render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+      begin
+        @delayed_job = Delayed::Job.find(params[:job_id])
+        percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
+        render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+      rescue ActiveRecord::RecordNotFound
+        render json: {not_found: "true"}.to_json
+      end
     end
-
+    
   end
 end
